66b68e88c18fff012eb310e0b8577ba7b723946f
catx0rr <catx0rr@gmail.com>
2020-08-19 06:10:22 +0800

    updated master

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="outer-start-page">
+    <div class="header-component">
+      <StartPage />
+    </div>
+
   <div id="start-page">
     <div class="pf-component">
       <Profile />
@@ -6,12 +11,12 @@
     </div>
     
     <div class="blog-content">
-      <StartPage />
       <nuxt-content :document="blog" />
     </div>
     <div class="rp-component">
       <RightPanel />
     </div>
+  </div>
   </div>
 </template>
 
@@ -33,71 +38,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-
-pre.line-numbers {
-  background: #343a40;
-  text-shadow: none;
-}
-
-span.token.operator {
-  background: #343a40;
-}
-
-div#home-page {
-  background: #343a40;
-  color: #fff;
-}
-
-.page-enter-active,
-.page-leave-active {
-  transition-property: opacity;
-  transition-timing-function: ease-in-out;
-  transition-duration: .5s;
-}
-
-.page-enter,
-.page-leave-to {
-  opacity: 0;
-}
-
-h1.blog-title {
-  text-align: center;
-  font-size: 2em;
-  margin: 0;
-}
-
-p.sub-title {
-  text-align: center;
-  font-size: .9em;
-  margin: 0;
-}
-
-article {
-  width: 100%;
-  margin: auto;
-  border-bottom: none;
-}
-
-p.content {
-  text-align: justify;
-  padding-bottom: .5em;
-}
-
-.content {
-  margin: auto;
-}
-
-
-@media only all and (max-width: 961px) {
-  div#content span.no-profile {
-    display: none;
-  }
-}
-
-html {
-  visibility: visible;
-}
-
-</style>
